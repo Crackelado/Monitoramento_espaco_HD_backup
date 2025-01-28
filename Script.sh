b01cb438d5cfe9143d6744b17c6231b8ff572b15
Luiz Thadeu Serafim <95077964+Crackelado@users.noreply.github.com>
2025-01-28 10:00:45 -0300

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-//Executa veri
+#Executa se verificar que o dia do mês é menor que 10
 if [[ $(date +%d) -lt 10 ]];
 then
 
+    #Se o dia da semana for terça-feira ou quinta-feira, coloca a descrição equivalente (há 2 HD's de backup, por dia, para cada servidor)
     if [[ $(date +%w) -eq 2 ]] || [[ $(date +%w) -eq 4 ]];
     then
         dia="TER/QUI"
@@ -11,10 +12,19 @@ then
         dia="SEG/QUA/SEX"
     fi
 
+    #Comando para montar unidade de rede na pasta "/mnt/backup"
     sudo mount -t cifs //10.150.200.7/h$ /mnt/backup -o username=****,password=****,iocharset=utf8
+
+    #Salta uma linha para as informações não ficarem todas juntas
     echo
+
+    #Exibi cabeçalho de qual HD e dia da semana é o backup
     echo "Espaço de armazenamento de $dia no HD de backup do servidor SRVX3"
+
+    #Exibi espaço ocupado e livre do HD
     df -h /mnt/backup
+
+    #Libera pasta e unidade de rede montada
     sudo umount /mnt/backup
 
     sudo mount -t cifs //10.150.200.8/g$ /mnt/backup -o username=****,password=****,iocharset=utf8
@@ -30,6 +40,8 @@ then
     sudo umount /mnt/backup
 
     echo
+
+    #Não fecha a janela do terminal esperando ação do usuário
     read -p "Pressione a tecla "ENTER" para fechar janela!"
 
 fi
